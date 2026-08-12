9c4b6a594cbd7bcf53644ad4a0ef39009b702b63
sergey_t <turushkin.s@mxm.email>
2026-08-12 20:00:42 +0300

    Fix BetBoom

--- betboom.js
+++ betboom.js
@@ -3,7 +3,12 @@ let allMarketGroups = document.querySelectorAll('section[class^="bb-"')
 let allMarketButtons = []
 let statisticWords = ['грок', 'углов', 'ЖЁЛТЫЕ', 'фолы', 'удары', 'офсайд', 'аут', 'сейв', 'отбор', 'передач', 'кто забьет', 'забьёт гол', 'автор', 'забьёт последний гол', 'забьёт и исход', 'карточки', 'забьёт 1-й', 'стартовый состав', 'число карточек']
 
-if (allMarketGroups[0].classList[0] != allMarketGroups[allMarketGroups.length-1].classList[0]) allMarketGroups[0].remove()
+if (allMarketGroups[0].classList[0] != allMarketGroups[allMarketGroups.length-1].classList[0]) {
+    allMarketGroups[0].remove()
+    allMarketGroups = document.querySelectorAll('section[class^="bb-"')
+} 
+
+
 
 for (let marketGroup of [...allMarketGroups]) {
     

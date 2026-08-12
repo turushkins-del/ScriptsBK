bee528a247adf6e2068526f3e4dbaab771e99dc3
sergey_t <turushkin.s@mxm.email>
2026-08-12 17:00:10 +0300

    last vbet

--- vbet.js
+++ vbet.js
@@ -2,26 +2,40 @@ let visibleMarketsContainers, lastVisibleMarketContainer, lastVisibleMarketConta
 let visibleMarketButtons = []
 
 // CONFIG
-let delayTimer = 3000
+let delayTimer = 1500
 
-while (true) {
+let shouldRun = true
+while (shouldRun) {
     visibleMarketsContainers = document.querySelectorAll('div[data-index]')
     for (visibleMarketContainer of [...visibleMarketsContainers]) {
+        let currentVisibleMarketsContainerDataIndex = parseInt(visibleMarketContainer.dataset.index)
         
-        let currentVisibleMarketsContainerDataIndex = visibleMarketContainer.dataset.index
+        if (visibleMarketContainer.nextElementSibling == null && currentVisibleMarketsContainerDataIndex == lastVisibleMarketContainerDataIndex) {
+            console.log(1)
+            shouldRun = false;
+            break
+        }
+
         if (currentVisibleMarketsContainerDataIndex <= lastVisibleMarketContainerDataIndex) continue 
+        
+        let showMoreBTN = visibleMarketContainer.querySelector('div > div > label > p')
+        if (showMoreBTN != null) {
+            showMoreBTN.click()
+            await new Promise(r => setTimeout(r, delayTimer))
+        }
 
         let visibleMarketContainerTitle = visibleMarketContainer.querySelector('.sgm-market-head-info').textContent
-        let visibleMarketsContainerButtons = visibleMarketContainer.querySelectorAll('.market-coefficient-bc')
-        console.log(visibleMarketContainerTitle)
-        console.log(visibleMarketsContainerButtons)
+        let visibleMarketsContainerButtons = visibleMarketContainer.querySelectorAll('.sgm-market-g-i-cell-bc')
+        // console.log(visibleMarketContainerTitle)
+        // console.log(visibleMarketsContainerButtons)
 
         visibleMarketButtons.push(...visibleMarketsContainerButtons)
         lastVisibleMarketContainer = visibleMarketContainer
         lastVisibleMarketContainerDataIndex = currentVisibleMarketsContainerDataIndex
+        console.log(lastVisibleMarketContainer.querySelector('.sgm-market-head-info').textContent)
+        console.log(lastVisibleMarketContainerDataIndex)
     }
-    console.log(lastVisibleMarketContainerDataIndex)
-    lastVisibleMarketContainer.scrollIntoView()
+    lastVisibleMarketContainer.scrollIntoView({block: "start"})
     await new Promise(r => setTimeout(r, delayTimer))
 }
 
